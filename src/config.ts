ef59dedfbca17f860ae68f6b087498361dabf9fa
HoseonRyu <HoseonRyu@users.noreply.github.com>
2026-03-05 06:09:44 +0000

    Prettified Code!

--- src/config.ts
+++ src/config.ts
@@ -6,6 +6,10 @@ export const config = {
   apiUrl: strip(process.env.CODLE_API_URL || "https://class.dev.codle.io"),
   port: parseInt(process.env.CODLE_PORT || "3000", 10),
   logLevel: process.env.CODLE_LOG_LEVEL || "INFO",
-  publicUrl: strip(process.env.CODLE_MCP_PUBLIC_URL || "https://mcp.dev.codle.io"),
-  authServerUrl: strip(process.env.CODLE_AUTH_SERVER_URL || "https://user.dev.codle.io"),
+  publicUrl: strip(
+    process.env.CODLE_MCP_PUBLIC_URL || "https://mcp.dev.codle.io",
+  ),
+  authServerUrl: strip(
+    process.env.CODLE_AUTH_SERVER_URL || "https://user.dev.codle.io",
+  ),
 };
